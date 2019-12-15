fea749f82327527cba7d548f1add3749da3431a3
DirectorKrennic <darragh.oneill9999@hotmail.com>
2019-12-15 16:00:14 +0000

    Worked on Sidebar Items, successfully rendering test data from Firebase

--- src/sidebaritem/sidebaritem.js
+++ src/sidebaritem/sidebaritem.js
@@ -1,20 +1,39 @@
 import React from 'react';
 import { withStyles } from '@material-ui/core/styles';
 import styles from './styles';
-import List from '@material-ui/core/List';
-import { Divider, Button } from '@material-ui/core';
-import SidebarItemComponent from '../sidebaritem/sidebaritem';
+import ListItem from '@material-ui/core/ListItem';
+import ListItemText from '@material-ui/core/ListItemText';
+import DeleteIcon from '@material-ui/icons/Delete';
+import { removeHTMLTags } from '../helpers';
 
-class SidebarComponent extends React.Component {
 
-    constructor(){
-        super();
-    }
+class SidebarItemComponent extends React.Component {
+
+
     render(){
+
+        const {_index, _note, classes, selectedNoteIndex} = this.props;
+
         return(
-            <div>Sidebar</div>
+            <div key={_index}>
+               <ListItem
+                className={classes.listItem}
+                selected={selectedNoteIndex === _index}
+                alignItems='flex-start'                   
+               >
+                   <div 
+                   className={classes.textSection}
+                   onClick={() => this.selectNote(_note, _index)}
+                   >
+                       <ListItemText
+                        primary={_note.title}
+                        secondary={_note.body.substring(0,25) + '...'}
+                       ></ListItemText>
+                   </div>
+               </ListItem>
+            </div>
         );
     }
 }
 
-export default withStyles(styles)(SidebarComponent);
+export default withStyles(styles)(SidebarItemComponent);
