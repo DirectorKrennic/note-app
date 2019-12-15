cf8e118de9525c35702b318fc861a131f8c0d1cb
DirectorKrennic <darragh.oneill9999@hotmail.com>
2019-12-15 17:48:15 +0000

    CRUD Operations completed

--- src/sidebar/sidebar.js
+++ src/sidebar/sidebar.js
@@ -6,89 +6,75 @@ import { Divider, Button } from '@material-ui/core';
 import SidebarItemComponent from '../sidebaritem/sidebaritem';
 
 class SidebarComponent extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      addingNote: false,
+      title: null
+    };
+  }
+  render() {
 
-    constructor(){
-        super();
-        this.state = {
-            addingNote: false,
-            title: null
-        };
+    const { notes, classes, selectedNoteIndex } = this.props;
+
+    if(notes) {
+      return(
+        <div className={classes.sidebarContainer}>
+          <Button
+            onClick={this.newNoteBtnClick}
+            className={classes.newNoteBtn}>{this.state.addingNote ? 'Cancel' : 'New Note'}</Button>
+            {
+              this.state.addingNote ? 
+              <div>
+                <input type='text'
+                  className={classes.newNoteInput}
+                  placeholder='Enter note title'
+                  onKeyUp={(e) => this.updateTitle(e.target.value)}>
+                </input>
+                <Button 
+                  className={classes.newNoteSubmitBtn}
+                  onClick={this.newNote}>Submit Note</Button>
+              </div> :
+              null
+            }
+            <List>
+              {
+                notes.map((_note, _index) => {
+                  return(
+                    <div key={_index}>
+                      <SidebarItemComponent
+                        _note={_note}
+                        _index={_index}
+                        selectedNoteIndex={selectedNoteIndex}
+                        selectNote={this.selectNote}
+                        deleteNote={this.deleteNote}>
+                      </SidebarItemComponent>
+                      <Divider></Divider>
+                    </div>
+                  )
+                })
+              }
+            </List>
+        </div>
+      );
+    } else {
+      return(<div></div>);
     }
-    render(){
+  }
 
-        //Destructuring 
-        const { notes, classes, selectedNoteIndex} = this.props;
+  newNoteBtnClick = () => {
+    this.setState({ title: null, addingNote: !this.state.addingNote });
+  }
+  updateTitle = (txt) => {
+    this.setState({ title: txt });
+  }
+  newNote = () => {
+    this.props.newNote(this.state.title);
+    this.setState({ title: null, addingNote: false });
+  }
+  selectNote = (n, i) => this.props.selectNote(n, i);
+  deleteNote = (note) => this.props.deleteNote(note);
 
-        if(notes){
-            return(
-                <div className={classes.sidebarContainer}>
-                    <Button
-                        onClick={this.newNoteBtnClick}
-                        className={classes.newNoteBtn}>
-                        {this.state.addingNote ? 'Cancel' : 'New Note'}               
-                    </Button>
-                    {
-                        this.state.addingNote ?
-                        <div>
-                            <input type="text"
-                            className={classes.newNoteInput}
-                            placeholder="Enter note title"
-                            onKeyUp={(e) => this.updateTitle(e.target.value)}>
-                            </input>
-                            <Button 
-                            className={classes.newNoteSubmitBtn}
-                            onClick={this.newNote}
-                            >Submit Note</Button>
-                        </div> :
-                        null
-                    }
-                    <List>
-                    {
-                        notes.map((_note, _index) =>{
-                        return(
-                            <div key={_index}>
-                                <SidebarItemComponent
-                                _note={_note}
-                                _index={_index}
-                                selectedNoteIndex={selectedNoteIndex}
-                                selectNote={this.selectNote}
-                                deleteNote={this.deleteNote}
-                                >
-                                </SidebarItemComponent>
-                                <Divider></Divider>
-                            </div>
-                        )
-                        }) 
-                    }
-                    </List>
-                </div>
-            );
-        }else{
-            return(<div></div>);
-        }
-    }
-
-    newNoteBtnClick = () => {
-        console.log('New Note Button clicked');
-        this.setState({ title: null, addingNote: !this.state.addingNote});
-    }
-
-    updateTitle = (txt) => {
-        console.log('User text', txt);
-        this.setState({ title: txt});
-    }
-
-    newNote = () => {
-        console.log(this.state);
-    } 
-
-    selectNote = (note, index) => {
-        this.props.selectNote(note, index);
-    }
-
-    deleteNote = () => {
-        console.loog('delete note');
-    }
 }
 
 export default withStyles(styles)(SidebarComponent);
